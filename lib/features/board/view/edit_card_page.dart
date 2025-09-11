66a7be5fb7fb2a11ccb6eb5e0549c396b4e41522
jarukit.bu <jarukit.bunchan@gmail.com>
2025-09-11 21:10:42 +0700

    fix: Remove accidental character from EditCardPage

--- lib/features/board/view/edit_card_page.dart
+++ lib/features/board/view/edit_card_page.dart
@@ -440,7 +440,7 @@ class _EditCardPageState extends State<EditCardPage> {
       _selectedCompany = 'none';
     }
   }
-
+ด
   Future<void> _loadWorkspaceUsers() async {
     try {
       final workspaceId = _controller.currentWorkspaceId.value;
